beb197afddc848e679041751460f0a13ca443633
sergeyesp <sergey_esp@i.ua>
2023-10-31 08:43:04 +0200

    Lesson2.2

---
+++ app/src/main/java/com/example/lookjson/models/MoviesModel.kt
@@ -0,0 +1,8 @@
+package com.example.lookjson.models
+
+data class MoviesModel(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
---
+++ app/src/main/java/com/example/lookjson/screens/main/MainFragmentViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.lookjson.screens.main
+
+class MainFragmentViewModel {
+}
