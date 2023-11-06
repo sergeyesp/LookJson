25011190e0923604d6dcd1c7a27bc24da061d9d2
sergeyesp <sergey_esp@i.ua>
2023-11-06 12:04:46 +0200

    Lesson5.1

--- app/src/main/java/com/example/lookjson/screens/main/MainFragment.kt
+++ app/src/main/java/com/example/lookjson/screens/main/MainFragment.kt
@@ -5,15 +5,35 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lookjson.R
 
 class MainFragment : Fragment() {
 
+    private var mBinding: FragmentMainBinding ?= null
+    private val binding get() = mBinding!!
+    lateinit var recyclerView: RecyclerView
+    private val adapter by lazy { MainAdapter() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        mBinding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
     }
 }
