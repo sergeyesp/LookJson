d835ff6753e5a290fa32eb02d874714a2d78a8a1
sergeyesp <sergey_esp@i.ua>
2023-11-08 10:51:08 +0200

    Lesson5.3

--- app/src/main/java/com/example/lookjson/screens/detail/DetailFragment.kt
+++ app/src/main/java/com/example/lookjson/screens/detail/DetailFragment.kt
@@ -5,14 +5,38 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lookjson.R
+import com.example.lookjson.databinding.FragmentDetailBinding
+import com.example.lookjson.databinding.FragmentMainBinding
+import com.example.lookjson.screens.favorite.FavoriteFragmentViewModel
+import com.example.lookjson.screens.main.MainAdapter
 
 class DetailFragment : Fragment() {
+
+    private var mBinding: FragmentDetailBinding?= null
+    private val binding get() = mBinding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        mBinding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        return mBinding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        val viewModel = ViewModelProvider(this)[DetailFragmentViewModel::class.java]
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
     }
 }
--- app/src/main/java/com/example/lookjson/screens/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/lookjson/screens/favorite/FavoriteFragment.kt
@@ -5,15 +5,42 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lookjson.R
+import com.example.lookjson.databinding.FragmentFavoriteBinding
+import com.example.lookjson.databinding.FragmentMainBinding
+import com.example.lookjson.screens.main.MainAdapter
+import com.example.lookjson.screens.main.MainFragmentViewModel
 
 class FavoriteFragment : Fragment() {
+
+    private var mBinding: FragmentFavoriteBinding?= null
+    private val binding get() = mBinding!!
+    lateinit var recyclerView: RecyclerView
+    private val adapter by lazy { MainAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    ): View {
+        mBinding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return mBinding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        val viewModel = ViewModelProvider(this)[FavoriteFragmentViewModel::class.java]
+        recyclerView = binding.rvFavorite
+        recyclerView.adapter = adapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
     }
 }
