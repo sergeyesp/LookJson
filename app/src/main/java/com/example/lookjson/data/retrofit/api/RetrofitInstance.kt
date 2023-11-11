6f3550bfe78e88738ccb4f6defd0c8be57edb7a1
sergeyesp <sergey_esp@i.ua>
2023-11-11 22:28:20 +0200

    Lesson7.2

--- app/src/main/java/com/example/lookjson/data/retrofit/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/lookjson/data/retrofit/api/RetrofitInstance.kt
@@ -1,4 +1,13 @@
 package com.example.lookjson.data.retrofit.api
 
+import com.example.lookjson.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+    }
 }
